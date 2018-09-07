ee8a538e9dff4bf69c342e53d9d5460089149838
Yonatan Cale <myselfandfredy@gmail.com>
2018-09-07 06:46:39 +0300

    Use new setings DEFINEs

--- backgammon/Player.cpp
+++ backgammon/Player.cpp
@@ -1,19 +1,16 @@
 #include "Player.h"
 #include "Settings.h"
 
-
-Settings s = Settings();
-
 Player::Player(int newPlayerId)
 {
 	id = newPlayerId;
 	switch (id)
 	{
 	case 1:
-		colour = s.Player1Colour;
+		Player1Colour;
 		break;
 	case 2:
-		colour = s.Player2Colour;
+		Player2Colour;
 		break;
 	default:
 		break;
