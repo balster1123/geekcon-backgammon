ff92ec75a0660e89b326bfbdb572ac3965ada9ed
Yonatan Cale <myselfandfredy@gmail.com>
2018-09-07 03:06:24 +0300

    +Line paramed constructor

--- backgammon/Line.cpp
+++ backgammon/Line.cpp
@@ -1,5 +1,10 @@
 #include "Line.h"
+#include "framework.h"
 
 Line::Line() : player(PLAYER_NONE), pieces(0)
+{
+}
+
+Line::Line(Players_t which_player, int num_pieces) : player(which_player), pieces(num_pieces)
 {
 }
--- backgammon/Line.h
+++ backgammon/Line.h
@@ -8,5 +8,6 @@ public:
     Players_t player; // The player who has pieces in this line
     int pieces; // How many pieces does this player have?
     Line();
+    Line(Players_t which_player, int num_pieces);
 };
 
