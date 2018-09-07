3a3d6c307787105c2ea722b25e0333d964540b47
Yonatan Cale <myselfandfredy@gmail.com>
2018-09-07 05:38:36 +0300

    +Basic joystick support, poll and print

--- backgammon/backgammon.ino
+++ backgammon/backgammon.ino
@@ -52,7 +52,47 @@ Adafruit_WS2801 strip = Adafruit_WS2801(4, dataPin, clockPin);
 //Adafruit_WS2801 strip = Adafruit_WS2801(25, dataPin, clockPin, WS2801_GRB);
 //Adafruit_WS2801 strip = Adafruit_WS2801(25, WS2801_GRB);
 
+
+// ------------------------------------------------------------------------------------
+
+// Thumb Joystick example
+// Mike Grusin, SparkFun Electronics 3/11
+// This code is free, baby. Use it however you like.
+
+// This sketch shows the basic operation of the Thumb Joystick (COM-09032) and breakout board (BOB-09110).
+// The joystick outputs two analog voltages (VERT and HORIZ), and one digital signal (SEL) for the pushbutton.
+
+// Connections to joystick (change if you use different pins):
+
+const int VERT = 0; // analog
+const int HORIZ = 1; // analog
+const int SEL = 2; // digital
+
+// Also connect the joystick VCC to Arduino 5V, and joystick GND to Arduino GND.
+
+// This sketch outputs serial data at 9600 baud (open Serial Monitor to view).
+
+// ------------------------------------------------------------------------------------
+
+
+
 void setup() {
+  // Serial:
+  Serial.begin(9600);
+  Serial.print("init1");
+  
+
+  // --------------------------------------------------------------------------------  
+  // JOYSTICK
+  // make the SEL line an input
+  pinMode(SEL,INPUT);
+  // turn on the pull-up resistor for the SEL line (see http://arduino.cc/en/Tutorial/DigitalPins)
+  digitalWrite(SEL,HIGH);
+  // --------------------------------------------------------------------------------
+
+
+  // --------------------------------------------------------------------------------  
+  // LED
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
   clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
 #endif
@@ -61,10 +101,36 @@ void setup() {
 
   // Update LED contents, to start they are all 'off'
   strip.show();
+  // --------------------------------------------------------------------------------  
 }
 
 
+void check_joystick() 
+{
+  int vertical, horizontal, select;
+  
+  // read all values from the joystick
+  
+  vertical = analogRead(VERT); // will be 0-1023
+  horizontal = analogRead(HORIZ); // will be 0-1023
+  select = digitalRead(SEL); // will be HIGH (1) if not pressed, and LOW (0) if pressed
+  
+  // print out the values
+  Serial.print("vertical: ");
+  Serial.print(vertical,DEC);
+  Serial.print(" horizontal: ");
+  Serial.print(horizontal,DEC);
+  Serial.print(" select: ");
+  if(select == HIGH)
+    Serial.println("not pressed");
+  else
+    Serial.println("PRESSED!");
+}  
+
 void loop() {
+  Serial.print("loop1");
+  check_joystick();
+  
   // Some example procedures showing how to display to the pixels
   
   colorWipe(Color(255, 0, 0), 50);
@@ -82,6 +148,7 @@ void rainbow(uint8_t wait) {
       strip.setPixelColor(i, Wheel( (i + j) % 255));
     }  
     strip.show();   // write all the pixels out
+    check_joystick();
     delay(wait);
   }
 }
@@ -101,6 +168,7 @@ void rainbowCycle(uint8_t wait) {
     }  
     strip.show();   // write all the pixels out
     delay(wait);
+    check_joystick();
   }
 }
 
